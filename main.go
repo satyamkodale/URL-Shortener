ee49ae21052cf1e59cf950538f2fce7b028310cb
satyamkodale <satyamkodale@gmail.com>
2025-03-22 16:10:37 +0530

    done with serversetup and root page handler

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 )
 
@@ -12,6 +13,31 @@ type URL struct {
 	CreationDate time.Time `json:"creation_date"`
 }
 
+func RootPageURL(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, world!")
+}
+
+func ShortURLHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func redirectURLHandler(w http.ResponseWriter, r *http.Request) {
+
+}
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println("Starting URL shortener...")
+	// OriginalURL := "https://github.com/Prince-1501/"
+	// generateShortURL(OriginalURL)
+
+	// Register the handler function to handle all requests to the root URL ("/")
+	http.HandleFunc("/", RootPageURL)
+	http.HandleFunc("/shorten", ShortURLHandler)
+	http.HandleFunc("/redirect/", redirectURLHandler)
+
+	// Start the HTTP server on port 8080
+	fmt.Println("Starting server on port 3000...")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Println("Error on starting server:", err)
+	}
 }
