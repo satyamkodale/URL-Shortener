a13c4085768667d1826af060a2e6c2651eed3c59
satyamkodale <satyamkodale@gmail.com>
2025-03-22 16:07:51 +0530

    created struct

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type URL struct {
+	ID           string    `json:"id"`
+	OriginalURL  string    `json:"original_url"`
+	ShortURL     string    `json:"short_url"`
+	CreationDate time.Time `json:"creation_date"`
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+}
